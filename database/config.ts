9800b9259a07c573ea87bf37cbb4c12b92637ab8
Federico Arguto <33673856+farguto@users.noreply.github.com>
2024-05-16 20:03:22 -0300

    Update config.ts

--- database/config.ts
+++ database/config.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const conectarDB = async (): Promise<void> =>{
     try {
-        await mongoose.connect("mongodb+srv://farguto:Fedillo1994%23@farguto.z0msweg.mongodb.net/")
+        await mongoose.connect("mongodb+srv://[username:password]:@farguto.z0msweg.mongodb.net/")
         console.log("Base de datos online");
     } catch (error) {
         console.log(error)
